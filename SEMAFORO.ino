7d2d2a9fec3a8fd20c10c1b386874fbea03bd280
Felipe <mda3012@gmail.com>
2023-11-15 10:01:06 -0300

    Semaforo v1

---
+++ SEMAFORO.ino
@@ -0,0 +1,14 @@
+int LEDverde=2;
+int LEDamarilla=3;
+int LEDroja=4;
+
+void setup() {
+  pinMode(LEDverde, OUTPUT);
+  pinMode(LEDamarilla, OUTPUT);
+  pinMode(Ledroja, OUTPUT);
+}
+
+void loop() {
+  digitalWrite();
+  delay();
+}
